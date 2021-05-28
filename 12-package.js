e6c0b5a0e6bd22060f4c224b3f9bfb35c01ea04d
Rey Cerio <rey.cerio@appetiser.com.au>
2021-05-28 13:44:37 -0700

    Setup git

--- 12-package.js
+++ 12-package.js
@@ -13,8 +13,18 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default flag)
 
+// npm install - install all the dependencies in package.json
+// npm i -D or --save-dev : to install dev dependencies
+// *** exit terminal ctrl + c to exit nodemon ***
+
+// npm uninstall packageName : to uninstall a package
+// nuclear uninstall: remove /node_modules, remove package-lock.json, reinstall based on package.json (npm install)
+
 // lodash in action
 const _ = require('lodash')
 const items = [1, [2, [3, [4, [5]]]]]
 const newItems = _.flattenDeep(items)
 console.log(newItems)
+
+// sudo npm install -g nodemon: in root directory to install globally
+// or use npx 
